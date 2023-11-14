215ece47a18494909bc26139cda459781110234d
Cepa95 <ceprnic.josip@gmail.com>
2023-11-14 18:58:40 +0100

    repository

--- OnlineTraining/src/main/java/com/training/OnlineTraining/repository/UserRepository.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.training.OnlineTraining.repository;
 
 import com.training.OnlineTraining.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
