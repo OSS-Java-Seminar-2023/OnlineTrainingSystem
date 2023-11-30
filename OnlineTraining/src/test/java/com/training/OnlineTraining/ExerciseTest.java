588ac8e9a81d576d99007092c4c4d316529109df
Luka Polic <luka_polic@epam.com>
2023-11-30 09:29:39 +0100

    Updated tests

--- OnlineTraining/src/test/java/com/training/OnlineTraining/ExerciseTest.java
+++ OnlineTraining/src/test/java/com/training/OnlineTraining/ExerciseTest.java
@@ -7,17 +7,13 @@ import com.training.OnlineTraining.service.ExerciseService;
 import org.flywaydb.test.FlywayTestExecutionListener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import java.util.List;
@@ -26,15 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-//@SpringBootTest
-//@TestPropertySource(locations = "classpath:application-test.properties")
-//@SpringJUnitConfig(classes = OnlineTrainingApplication.class)
-//@ExtendWith(SpringExtension.class)
-//@AutoConfigureMockMvc
-//@TestExecutionListeners({
-//		DependencyInjectionTestExecutionListener.class,
-//		FlywayTestExecutionListener.class
-//})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(classes = OnlineTrainingApplication.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -59,8 +46,7 @@ public class ExerciseTest {
 	public void testCreateAddress() {
 
 		ExerciseDTO exerciseDTO = new ExerciseDTO("Exercise 1", "Description 1", "NO", "LOW");
-		ResponseEntity<Exercise> responseEntity =
-				(ResponseEntity<Exercise>) exerciseController.createExercise(exerciseDTO);
+		ResponseEntity<Exercise> responseEntity = exerciseController.createExercise(exerciseDTO);
 
 		assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
 		assertNotNull(responseEntity.getBody());
