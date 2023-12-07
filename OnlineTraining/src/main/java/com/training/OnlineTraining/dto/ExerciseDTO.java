ab4f442bc05455fa6987e914b7945fc13ddfc2e6
Luka Polic <luka_polic@epam.com>
2023-12-07 19:52:31 +0100

    Changed exercise REST controller to ordinardy controller and changed test to service layer testing

--- OnlineTraining/src/main/java/com/training/OnlineTraining/dto/ExerciseDTO.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/dto/ExerciseDTO.java
@@ -2,9 +2,11 @@ package com.training.OnlineTraining.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExerciseDTO {
     private String name;
     private String description;
