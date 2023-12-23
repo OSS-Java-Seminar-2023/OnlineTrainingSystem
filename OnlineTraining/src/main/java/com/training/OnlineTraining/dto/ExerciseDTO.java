8d8fca712d71d899703c2ec7d97e75c399028106
Luka Polic <luka_polic@epam.com>
2023-12-23 11:04:23 +0100

    Added thymeleaf templated for exercise

--- OnlineTraining/src/main/java/com/training/OnlineTraining/dto/ExerciseDTO.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/dto/ExerciseDTO.java
@@ -3,11 +3,16 @@ package com.training.OnlineTraining.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ExerciseDTO {
+    private UUID id;
     private String name;
     private String description;
     private String equipmentNeeded;
