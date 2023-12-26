4a4818c44603db14f3d53188a94c0dad10696107
Luka Polic <luka_polic@epam.com>
2023-12-26 15:27:29 +0100

    Added updates for workout, updated logs

--- OnlineTraining/src/main/java/com/training/OnlineTraining/controller/ExerciseController.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/controller/ExerciseController.java
@@ -4,6 +4,8 @@ import com.training.OnlineTraining.dto.ExerciseDTO;
 import com.training.OnlineTraining.mapper.ExerciseMapper;
 import com.training.OnlineTraining.model.Exercise;
 import com.training.OnlineTraining.service.ExerciseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,59 +19,65 @@ import java.util.UUID;
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
-
     private final ExerciseMapper exerciseMapper;
+    private static final Logger logger = LoggerFactory.getLogger(ExerciseController.class);
 
+    @Autowired
     public ExerciseController(ExerciseService exerciseService, ExerciseMapper exerciseMapper) {
         this.exerciseService = exerciseService;
         this.exerciseMapper = exerciseMapper;
+        logger.info("ExerciseController initialized.");
     }
 
     @GetMapping("/create")
     public String showAddExerciseForm(Model model) {
+        logger.info("Displaying add exercise form.");
         model.addAttribute("exercise", new ExerciseDTO());
         return "exercise/exerciseForm";
     }
 
     @PostMapping("/create")
     public String createExercise(@ModelAttribute ExerciseDTO exerciseDTO, Model model) {
+        logger.info("Creating a new exercise.");
         Exercise createdExercise = exerciseService.createExercise(exerciseDTO);
         return "redirect:/exercise";
     }
 
     @GetMapping("/details/{id}")
     public String showExerciseDetails(@PathVariable UUID id, Model model) {
+        logger.info("Displaying exercise details for ID: {}", id);
         Exercise exercise = exerciseService.getExerciseById(id);
         model.addAttribute("exercise", exercise);
-        return "exercise/exerciseDetails"; // Create a details view
+        return "exercise/exerciseDetails";
     }
-
 
     @GetMapping()
     public String getAllExercises(Model model) {
+        logger.info("Fetching all exercises.");
         List<Exercise> exercises = exerciseService.getAllExercises();
         model.addAttribute("exercises", exercises);
-        return "exercise/exerciseList"; // Return the Thymeleaf view name
+        return "exercise/exerciseList";
     }
 
     @GetMapping("/update/{id}")
     public String getUpdateFormForExercise(@PathVariable UUID id, Model model) {
+        logger.info("Displaying update form for exercise ID: {}", id);
         Exercise tempExercise = exerciseService.getExerciseById(id);
-        System.out.println("getUpdateFormForExercise ->  exercise : " + tempExercise);
         model.addAttribute("exerciseForUpdating", tempExercise);
-        return "exercise/exerciseUpdateForm"; // Return the Thymeleaf view name
+        return "exercise/exerciseUpdateForm";
     }
 
     @PostMapping("/update/{id}")
     public String updateExercise(@PathVariable String id, @ModelAttribute("exercise") ExerciseDTO exerciseDTO, Model model) {
-        System.out.println("updateExercise -> " + id);
-        System.out.println("updateExercise -> " + exerciseDTO);
+        logger.info("Updating exercise for ID: {}", id);
+        logger.info("ExerciseDTO: {}", exerciseDTO);
         exerciseService.updateExercise(exerciseDTO.getId(), exerciseDTO);
         return "redirect:/exercise";
     }
 
     @PostMapping("/delete/{id}")
     public String deleteExercise(@PathVariable UUID id) {
+        logger.info("Deleting exercise for ID: {}", id);
         exerciseService.deleteExercise(id);
         return "redirect:/exercise";
     }
