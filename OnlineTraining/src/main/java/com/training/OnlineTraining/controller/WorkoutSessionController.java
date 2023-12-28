9140cc30119bd37f8559aa0b1806ca99e01c5334
Luka Polic <luka_polic@epam.com>
2023-12-28 10:30:55 +0100

    Added deleting

--- OnlineTraining/src/main/java/com/training/OnlineTraining/controller/WorkoutSessionController.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/controller/WorkoutSessionController.java
@@ -19,7 +19,6 @@ public class WorkoutSessionController {
 
 	private static final Logger logger = LoggerFactory.getLogger(WorkoutSessionController.class);
 
-
 	public WorkoutSessionController(WorkoutSessionService workoutSessionService) {
 		this.workoutSessionService = workoutSessionService;
 	}
@@ -30,12 +29,27 @@ public class WorkoutSessionController {
 			@ModelAttribute("workout") WorkoutDTO workoutDTO,
 			HttpSession session) {
 
+		logger.info("Updating workout and sessions for workout ID: {}", workoutID);
+
 		workoutSessionService.updateWorkoutSessions(workoutDTO.getWorkoutSessions());
+
+		logger.info("Workout and sessions updated successfully.");
 
 		return "redirect:/workout/details/" + workoutID;
 	}
 
+	@PostMapping("/delete/{workoutSessionID}/{workoutID}")
+	public String deleteWorkoutSession(@PathVariable String workoutSessionID, @PathVariable String workoutID) {
+		logger.info("Deleting workout session with ID: {}", workoutSessionID);
 
+		workoutSessionService.deleteWorkoutSession(UUID.fromString(workoutSessionID));
+
+		logger.info("Workout session deleted successfully.");
+
+		// Redirect to the appropriate page after deletion
+		return "redirect:/workout/details/" + UUID.fromString(workoutID); // Assuming the path is correct
+	}
 
 
 }
+
