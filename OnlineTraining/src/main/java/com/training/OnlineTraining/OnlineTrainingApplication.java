d62640f3dc24c9f8a1e17d5b3d3a1732b45648ef
Ceprnic <ceprnic.josip@gmail.com>
2023-12-29 14:59:39 +0100

    improved mail, put it on different thread

--- OnlineTraining/src/main/java/com/training/OnlineTraining/OnlineTrainingApplication.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/OnlineTrainingApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @ComponentScan(basePackages = "com.training.OnlineTraining")
 @SpringBootApplication
 @EntityScan("com.training.OnlineTraining.model")
+@EnableAsync
 public class OnlineTrainingApplication {
 
 	public static void main(String[] args) {
--- OnlineTraining/src/main/java/com/training/OnlineTraining/service/implementation/MailServiceImpl.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/service/implementation/MailServiceImpl.java
@@ -7,16 +7,26 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @AllArgsConstructor
 public class MailServiceImpl implements MailService {
     private final JavaMailSender javaMailSender;
 
+    @Async
+    @Override
+    public CompletableFuture<Void> sendEmailAsync(String toEmail, String body, String subject) throws MessagingException {
+        sendEmail(toEmail, body, subject);
+        System.out.println("Sent email asynchronously");
+        return CompletableFuture.completedFuture(null); // or simply return new CompletableFuture<>();
+    }
+
     @Override
     public void sendEmail(String toEmail, String body, String subject) throws MessagingException {
-
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
 
