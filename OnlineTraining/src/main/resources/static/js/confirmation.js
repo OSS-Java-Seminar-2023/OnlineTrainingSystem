3b88194d9e8173f8b6cc5e429f89870ff940ac31
Ceprnic <ceprnic.josip@gmail.com>
2023-12-31 13:32:20 +0100

    better UI, user can now leave the pop up without pressing no, but instead pressing anything on the page, done excerciseList css

--- OnlineTraining/src/main/resources/static/js/confirmation.js
+++ OnlineTraining/src/main/resources/static/js/confirmation.js
@@ -2,6 +2,9 @@ function showConfirmation(form) {
     document.getElementById('confirmation-dialog').style.display = 'block';
 
     window.deleteForm = form;
+
+    document.addEventListener('click', clickOutsideDialog);
+
     return false;
 }
 
@@ -11,8 +14,22 @@ function confirmDelete() {
     if (window.deleteForm) {
         window.deleteForm.submit();
     }
+    document.removeEventListener('click', clickOutsideDialog);
 }
 
 function cancelDelete() {
     document.getElementById('confirmation-dialog').style.display = 'none';
+
+    document.removeEventListener('click', clickOutsideDialog);
+}
+
+function clickOutsideDialog(event) {
+    var confirmationDialog = document.getElementById('confirmation-dialog');
+
+    if (!confirmationDialog.contains(event.target)) {
+        document.getElementById('confirmation-dialog').style.display = 'none';
+
+
+        document.removeEventListener('click', clickOutsideDialog);
+    }
 }
