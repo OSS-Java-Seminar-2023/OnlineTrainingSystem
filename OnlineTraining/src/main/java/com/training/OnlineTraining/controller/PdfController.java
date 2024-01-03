13010e15b52f6521096e82a5cdd17937731bd595
Luka <lpolic554@gmail.com>
2024-01-03 12:51:44 +0100

    Updated pdf generator

--- OnlineTraining/src/main/java/com/training/OnlineTraining/controller/PdfController.java
+++ OnlineTraining/src/main/java/com/training/OnlineTraining/controller/PdfController.java
@@ -1,33 +1,45 @@
 package com.training.OnlineTraining.controller;
 
+import com.training.OnlineTraining.dto.output.WorkoutOutputDTO;
 import com.training.OnlineTraining.service.PdfGenerator;
+import com.training.OnlineTraining.service.WorkoutService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class PdfController {
 
+	private final WorkoutService workoutService;
+
+	public PdfController(WorkoutService workoutService) {
+		this.workoutService = workoutService;
+	}
+
 	@GetMapping("/show-pdf")
 	public String showPdf(Model model) {
-		// Add data to be displayed in the Thymeleaf template
-		model.addAttribute("message", "Hello, this is data for the PDF!");
 
-		// Return the name of the Thymeleaf template to be rendered
-		return "pdf/pdf_template";
+		List<WorkoutOutputDTO> workoutOutputDTOList = workoutService.getAllWorkouts();
+
+		model.addAttribute("workout", workoutOutputDTOList.get(0));
+
+		return "pdf/workout_pdf_template";
 	}
 
 	@PostMapping("/generate-pdf")
-	public void generatePdf(@RequestParam("htmlContent") String htmlContent, HttpServletResponse response) throws IOException {
-		// Convert HTML content to PDF and allow download
+	public void generatePdf(@ModelAttribute WorkoutOutputDTO workout, HttpServletResponse response) throws IOException {
+		// Convert WorkoutOutputDTO object to PDF and allow download
 		response.setContentType("application/pdf");
 		response.setHeader("Content-Disposition", "attachment; filename=generated-pdf.pdf");
 
-		PdfGenerator.generatePdfFromHtml(htmlContent, response.getOutputStream());
+		PdfGenerator.generatePdfFromHtml(workout, response.getOutputStream());
 	}
+
 }
