ce34641fd777761ac0232dd6c872573d4e2f8656
Ceprnic <ceprnic.josip@gmail.com>
2024-01-17 01:21:22 +0100

    removed inline js

--- OnlineTraining/src/main/resources/static/js/chart.js
+++ OnlineTraining/src/main/resources/static/js/chart.js
@@ -1,121 +1,119 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var ctx = document.getElementById('bodyWeightChart').getContext('2d');
-    var measurements = JSON.parse(document.getElementById('bodyWeightChart').getAttribute('data-measurements'));
+    /*<![CDATA[*/
+    document.addEventListener('DOMContentLoaded', function () {
+        let ctx = document.getElementById('bodyWeightChart').getContext('2d');
 
-    console.log("Measurements:", measurements);
+        if (measurements) {
+            var bodyWeightData = measurements.map(function (measurement, index) {
+                return {
+                    x: index + 1,
+                    y: measurement.bodyWeight
+                };
+            });
 
-    if (measurements) {
-        var bodyWeightData = measurements.map(function (measurement, index) {
-            return {
-                x: index + 1,
-                y: measurement.bodyWeight
-            };
-        });
+            let bodyFatData = measurements.map(function (measurement, index) {
+                return {
+                    x: index + 1,
+                    y: measurement.bodyFat
+                };
+            });
 
-        var bodyFatData = measurements.map(function (measurement, index) {
-            return {
-                x: index + 1,
-                y: measurement.bodyFat
-            };
-        });
+            let waistCircumferenceData = measurements.map(function (measurement, index) {
+                return {
+                    x: index + 1,
+                    y: measurement.waistCircumference
+                };
+            });
 
-        var waistCircumferenceData = measurements.map(function (measurement, index) {
-            return {
-                x: index + 1,
-                y: measurement.waistCircumference
-            };
-        });
+            let chestCircumferenceData = measurements.map(function (measurement, index) {
+                return {
+                    x: index + 1,
+                    y: measurement.chestCircumference
+                };
+            });
 
-        var chestCircumferenceData = measurements.map(function (measurement, index) {
-            return {
-                x: index + 1,
-                y: measurement.chestCircumference
-            };
-        });
+            let armCircumferenceData = measurements.map(function (measurement, index) {
+                return {
+                    x: index + 1,
+                    y: measurement.armCircumference
+                };
+            });
 
-        var armCircumferenceData = measurements.map(function (measurement, index) {
-            return {
-                x: index + 1,
-                y: measurement.armCircumference
-            };
-        });
+            let legCircumferenceData = measurements.map(function (measurement, index) {
+                return {
+                    x: index + 1,
+                    y: measurement.legCircumference
+                };
+            });
 
-        var legCircumferenceData = measurements.map(function (measurement, index) {
-            return {
-                x: index + 1,
-                y: measurement.legCircumference
-            };
-        });
-
-        var bodyWeightChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                datasets: [
-                    {
-                        label: 'Body Weight',
-                        data: bodyWeightData,
-                        backgroundColor: '#33cc77',
-                        borderWidth: 2
-                    },
-                    {
-                        label: 'Body Fat',
-                        data: bodyFatData,
-                        backgroundColor: '#6fa8dc',
-                        borderWidth: 2
-                    },
-                    {
-                        label: 'Waist Circumference',
-                        data: waistCircumferenceData,
-                        backgroundColor: '#ffcc00',
-                        borderWidth: 2,
-                        hidden: true
-                    },
-                    {
-                        label: 'Chest Circumference',
-                        data: chestCircumferenceData,
-                        backgroundColor: '#ff6666',
-                        borderWidth: 2,
-                        hidden: true
-                    },
-                    {
-                        label: 'Arm Circumference',
-                        data: armCircumferenceData,
-                        backgroundColor: '#9966ff',
-                        borderWidth: 2,
-                        hidden: true
-                    },
-                    {
-                        label: 'Leg Circumference',
-                        data: legCircumferenceData,
-                        backgroundColor: '#ff9900',
-                        borderWidth: 2,
-                        hidden: true
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    x: {
-                        type: 'linear',
-                        position: 'bottom',
-                        ticks: {
-                            stepSize: 1
+            let bodyWeightChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    datasets: [
+                        {
+                            label: 'Body Weight',
+                            data: bodyWeightData,
+                            backgroundColor: '#33cc77',
+                            borderWidth: 2
                         },
-                        title: {
-                            display: true,
-                            text: 'Measurement'
+                        {
+                            label: 'Body Fat',
+                            data: bodyFatData,
+                            backgroundColor: '#6fa8dc',
+                            borderWidth: 2
+                        },
+                        {
+                            label: 'Waist Circumference',
+                            data: waistCircumferenceData,
+                            backgroundColor: '#ffcc00',
+                            borderWidth: 2,
+                            hidden: true
+                        },
+                        {
+                            label: 'Chest Circumference',
+                            data: chestCircumferenceData,
+                            backgroundColor: '#ff6666',
+                            borderWidth: 2,
+                            hidden: true
+                        },
+                        {
+                            label: 'Arm Circumference',
+                            data: armCircumferenceData,
+                            backgroundColor: '#9966ff',
+                            borderWidth: 2,
+                            hidden: true
+                        },
+                        {
+                            label: 'Leg Circumference',
+                            data: legCircumferenceData,
+                            backgroundColor: '#ff9900',
+                            borderWidth: 2,
+                            hidden: true
                         }
-                    },
-                    y: {
-                        beginAtZero: true,
-                        title: {
-                            display: true,
-                            text: 'kg, kg/cm'
+                    ]
+                },
+                options: {
+                    scales: {
+                        x: {
+                            type: 'linear',
+                            position: 'bottom',
+                            ticks: {
+                                stepSize: 1
+                            },
+                            title: {
+                                display: true,
+                                text: 'Measurement'
+                            }
+                        },
+                        y: {
+                            beginAtZero: true,
+                            title: {
+                                display: true,
+                                text: 'kg/cm'
+                            }
                         }
                     }
                 }
-            }
-        });
-    }
-});
-
+            });
+        }
+    });
+    /*]]>*/
